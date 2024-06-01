afc2cdd8bddb60cb2c00c191b86fc5735cff74d7
madhu-888 <madhuspediredla@gmail.com>
2024-06-01 19:03:21 +0530

    fixed social apps icons

--- src/app/modules/profile/components/details/details.component.ts
+++ src/app/modules/profile/components/details/details.component.ts
@@ -27,12 +27,12 @@ export class DetailsComponent {
     realName: 'Madhu Sudhan Rao',
     userName: '@madhu._.p',
     details: [
-      { icon: 'mail', value: 'madhuspediredla@gmail.com' },
-      { icon: 'phone', value: '7382036544' },
-      { icon: 'instagram', value: 'Instagram', url: 'https://instagram.com/madhuu._.p?igsh=ZmpjdWtqOGlhcWp5&utm_source=qr'},
-      { icon: 'github_circle', value: 'Github', url: 'https://github.com/madhu-sudhan-rao'},
-      { icon: 'linkedin-box', value: 'Linkedin', url: 'https://www.linkedin.com/in/madhu-sudhan-rao-pediredla-18588a1a3/'},
-      { icon: 'twitter', value: 'Twitter', url: 'https://twitter.com/madhu_pediredla'},
+      { icon: 'pi pi-envelope', value: 'madhuspediredla@gmail.com' },
+      { icon: 'pi pi-phone', value: '7382036544' },
+      { icon: 'pi pi-instagram', value: 'Instagram', url: 'https://instagram.com/madhuu._.p?igsh=ZmpjdWtqOGlhcWp5&utm_source=qr'},
+      { icon: 'pi pi-github', value: 'Github', url: 'https://github.com/madhu-sudhan-rao'},
+      { icon: 'pi pi-linkedin', value: 'Linkedin', url: 'https://www.linkedin.com/in/madhu-sudhan-rao-pediredla-18588a1a3/'},
+      { icon: 'pi pi-twitter', value: 'Twitter', url: 'https://twitter.com/madhu_pediredla'},
       { icon: 'snapchat', value: 'Snapchat', url: 'https://snapchat.com/add/madhu5815?share_id=ycdHba8OS3-g00WZoXr4fA&locale=en_IN'},
       
     ]
