2e4fe4fa0b4d89039772e3f195e5042527446705
unknown <madhuspediredla@gmail.com>
2024-06-09 10:14:26 +0530

    changes add

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -6,13 +6,21 @@ import { SharedModule } from '../../shared/shared.module';
 import { ProfileScreenComponent } from './screens/profile-screen/profile-screen.component';
 import { BottomSheetComponent } from './components/bottom-sheet/bottom-sheet.component';
 import { DetailsComponent } from './components/details/details.component';
+import { EmptyProfileDetailsViewComponent } from './components/empty-profile-details-view/empty-profile-details-view.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { UserDetailRowComponent } from './components/user-details/user-detail-row/user-detail-row.component';
+import { UserNameCardComponent } from './components/user-name-card/user-name-card.component';
 
 
 @NgModule({
   declarations: [
     ProfileScreenComponent,
     DetailsComponent,
-    BottomSheetComponent
+    BottomSheetComponent,
+    EmptyProfileDetailsViewComponent,
+    UserDetailsComponent,
+    UserDetailRowComponent,
+    UserNameCardComponent
   ],
   imports: [
     CommonModule,
